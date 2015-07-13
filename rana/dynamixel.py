579544b42a2817c2a307229c6e6eb03e055dabd5
akash9182 <akash9182akash@gmail.com>
2015-07-14 00:49:23 +0530

    dyna read and write

--- rana/dynamixel.py
+++ rana/dynamixel.py
@@ -1,10 +1,11 @@
 
-def Dyna_code ():
-	#int GO_TO_DYNA_1_POS
-	#int GO_TO_DYNA_2_POS
+def Dyna_code():
+	GO_TO_DYNA_1_POS
+	GO_TO_DYNA_2_POS
+	print()
 
-def dyna_write():
-	print("dynamixel writing:")
+def dyna_write() :
+	print('dynamixel write:')
 
 
 def	dyna_read() :
