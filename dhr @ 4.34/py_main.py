036f08b294cae2bd4fc5497c8fcd540f4f6b1a46
dhr9 <dhr.reshamwala@gmail.com>
2015-07-14 05:29:04 +0530

    badhu chaale chhe

--- dhr @ 4.34/py_main.py
+++ dhr @ 4.34/py_main.py
@@ -17,20 +17,28 @@ def modify_blocks():
 	print("-----------------")
 	for i in range (CURRENT_ARRAY_LENGTH):
 		print(i)
-		print("LOOKUP_OUTPUT = ",lookup.LOOKUP_OUTPUT)
-		print("DYNA_1_POS = ",lookup.DYNA_1_POS)
-		print("DYNA_2_POS = ",lookup.DYNA_2_POS)
-		print("Letter = ",CURRENT_ARRAY[i])
+		# print("LOOKUP_OUTPUT = ",lookup.LOOKUP_OUTPUT)
+		# print("DYNA_1_POS = ",lookup.DYNA_1_POS)
+		# print("DYNA_2_POS = ",lookup.DYNA_2_POS)
+		print("Picking ",CURRENT_ARRAY[i])
 		lookup.lookup(CURRENT_ARRAY[i],0,0)
-		# eg:- "A",pick,fwd
-		print("LOOKUP_OUTPUT = ",lookup.LOOKUP_OUTPUT)
+		# # eg:- "A",pick,fwd
+		# print("LOOKUP_OUTPUT = ",lookup.LOOKUP_OUTPUT)
 		dynamixel.GO_TO_DYNA_1_POS = lookup.LOOKUP_OUTPUT[0]
 		dynamixel.GO_TO_DYNA_2_POS = lookup.LOOKUP_OUTPUT[1]
 		dynamixel.dyna_write()
 		lookup.DYNA_1_POS = dynamixel.GO_TO_DYNA_1_POS
 		lookup.DYNA_2_POS = dynamixel.GO_TO_DYNA_2_POS
-		print("DYNA_1_POS = ",lookup.DYNA_1_POS)
-		print("DYNA_2_POS = ",lookup.DYNA_2_POS)
-		print("----")
-
-	print("-----------------")
+		# print("DYNA_1_POS = ",lookup.DYNA_1_POS)
+		# print("DYNA_2_POS = ",lookup.DYNA_2_POS)
+		# print("----")
+		print("-----------------")
+		print("Placing ",CURRENT_ARRAY[i])
+		lookup.lookup(CURRENT_ARRAY[i],1,0)
+		dynamixel.GO_TO_DYNA_1_POS = lookup.LOOKUP_OUTPUT[0]
+		dynamixel.GO_TO_DYNA_2_POS = lookup.LOOKUP_OUTPUT[1]
+		dynamixel.dyna_write()
+		lookup.DYNA_1_POS = dynamixel.GO_TO_DYNA_1_POS
+		lookup.DYNA_2_POS = dynamixel.GO_TO_DYNA_2_POS
+		
+		print("-----------------")
