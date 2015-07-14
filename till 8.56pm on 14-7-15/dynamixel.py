2c6ee450a73dcd2f9e16286bd307e9653bbf2d3f
dhr9 <dhr.reshamwala@gmail.com>
2015-07-14 20:57:04 +0530

    just in case

---
+++ till 8.56pm on 14-7-15/dynamixel.py
@@ -0,0 +1,19 @@
+from debug import debug
+import exception_handling
+
+GO_TO_DYNA_1_POS=0
+GO_TO_DYNA_2_POS=0
+
+@debug()	
+def dyna_write() :
+	global GO_TO_DYNA_1_POS
+	global GO_TO_DYNA_2_POS
+	print("moving to ",GO_TO_DYNA_1_POS,",",GO_TO_DYNA_2_POS)
+	for i in range(2):
+		dyna_read()
+	print("reached !")
+
+
+#@debug()
+def	dyna_read() :
+	print("reading...")
