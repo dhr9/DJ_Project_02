0c74ca7a579354c22c51b7c11f53e9857c3c5dee
dhr9 <dhr.reshamwala@gmail.com>
2015-07-14 22:01:20 +0530

    arduino small changes

--- till 8.56pm on 14-7-15/arduino.py
+++ till 8.56pm on 14-7-15/arduino.py
@@ -2,3 +2,12 @@ import serial
 
 SERVO_ANGLE = 0
 
+def startup(com) :
+    ser = serial.Serial(port = com)      #create an instance of the serial.Serial class 
+    print(ser)
+    ser.baudrate = 57600                 #set baudrate equal to 9600
+    print(ser.baudrate)
+    return ser
+
+
+#arduino =  startup('com3')
--- till 8.56pm on 14-7-15/dynamixel.py
+++ till 8.56pm on 14-7-15/dynamixel.py
@@ -2,8 +2,20 @@ from debug import debug
 import exception_handling
 import arduino
 
+import time
+import serial
+
 GO_TO_DYNA_1_POS=0
 GO_TO_DYNA_2_POS=0
+
+def startup(com) :
+    ser = serial.Serial(port = com)      #create an instance of the serial.Serial class 
+    print(ser)
+    ser.baudrate = 57600                 #set baudrate equal to 9600
+    print(ser.baudrate)
+    return ser
+
+#dynamixel = startup('com4')
 
 @debug()	
 def dyna_write() :
