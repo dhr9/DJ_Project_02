20269c65cd02aa65993cbf285f4ee6d220ab5fdf
dhr9 <dhr.reshamwala@gmail.com>
2015-12-25 21:31:43 +0530

    dhr_mark6 first commit
    Checked files:
    gui.py
    dynamixel.py
    error_logging.py
    exception_handling.py
    serial_ports_setup.py
    status_packet_handling.py
    string_handling.py
    debug.py

---
+++ dhr_mark6/subordinate_directory/error_logging.py
@@ -0,0 +1,97 @@
+import time
+import sys
+gui = sys.modules['__main__']
+
+def log(error) :
+
+    class get_time() :
+
+        def __init__(self) :
+            self.t = ''
+            self.year = ''
+            self.month = ''
+            self.day = ''
+            self.hour = ''
+            self.minute = ''
+            self.second = ''
+            self.week_day = ''
+
+            self.get_time()
+
+        def __print__(self) :
+            print('time : ',self.t)
+            print('year : ',self.year)
+            print('month : ',self.month)
+            print('day : ',self.day)
+            print('hour : ',self.hour)
+            print('minute : ',self.minute)
+            print('second : ',self.second)
+            print('week_day : ',self.week_day)
+
+        def get_time(self) :
+
+            def get_before_and_after(string,after,before) :
+                
+                dont_need_character_list = [' ']
+                if((after in string) and (before in string)) :
+                    i = string.index(after) + len(after)
+                    j = string.index(before,i,i+5)
+                    return_string = ''
+                    for k in range(i,j) :
+                        if(string[k] not in dont_need_character_list) :
+                            return_string += string[k]
+                    return return_string
+                else :
+                    print('before or after not in string')
+
+            def one_character_to_two_character(string) : 
+                if len(string) == 1 : 
+                    return('0' + string)
+                else : 
+                    return(string)
+                    
+            self.t = str(time.localtime())
+            self.year = get_before_and_after(self.t,'tm_year=',',')
+            self.month = get_before_and_after(self.t,'tm_mon=',',')
+            self.day = get_before_and_after(self.t,'tm_mday=',',')
+            
+            self.hour = one_character_to_two_character(get_before_and_after(self.t,'tm_hour=',','))
+            self.minute = one_character_to_two_character(get_before_and_after(self.t,'tm_min=',','))
+            self.second = one_character_to_two_character(get_before_and_after(self.t,'tm_sec=',','))
+            
+            self.week_day = get_before_and_after(self.t,'tm_wday=',',')
+            day = {
+                '00':'Mon',
+                '01':'Tue',
+                '02':'Wed',
+                '03':'Thu',
+                '04':'Fri',
+                '06':'Sat',
+                '07':'Sun'
+            }
+            month = {
+                '01':'Jan',
+                '02':'Feb',
+                '03':'Mar',
+                '04':'Apr',
+                '05':'May',
+                '06':'Jun',
+                '07':'Jul',
+                '08':'Aug',
+                '09':'Sep',
+                '10':'Aug',
+                '11':'Nov',
+                '12':'Dec'
+            }
+
+            self.week_day = day.get(self.week_day)
+            self.month = month.get(self.month)
+
+    t = get_time()
+    return_string = t.day + '/' + t.month + '/' + t.year \
+    + ' ' + t.hour + ':' + t.minute + ':' + t.second\
+    + ' --> ' + error + '\n'
+
+    with open(gui.WORKING_DIRECTORY + '/log.txt','a') as log: 
+        log.write(return_string)
+        
---
+++ dhr_mark6/subordinate_directory/exception_handling.py
@@ -0,0 +1,32 @@
+import sys
+gui = sys.modules['__main__']
+import error_logging
+
+def handle_exception(module_name,exception_name,*args) :
+	
+	arduino = ['arduino not responding','cant connect']
+	dynamixel = ['dynamixel not responding','cant connect']
+	lookup = []
+	status_packet_handling  = ['input voltage error','angle limit error',\
+	'overheting error','range error','checksum error','overload error',\
+	'instruction error','invalid error byte']
+	py_main = []
+
+	module_names = {
+		'arduino' : arduino,
+		'dynamixel' : dynamixel,
+		'lookup' : lookup,
+		'status_packet_handling' : status_packet_handling,
+		'py_main' : py_main,
+	}
+
+	module_name = module_name.split('.')[-1]
+
+	if module_name in module_names.keys() and\
+		exception_name in module_names[module_name] :
+		print('Exception :' + module_name + '--> ' + exception_name)
+		error_logging.log(module_name + '--> ' + exception_name)
+		# CHANGE -- Implement functions in GUI and call from here
+	else : 
+		print('invalid exception')
+		# CHANGE -- Let GUI print this in a msg box
